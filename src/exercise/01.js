592076d79c6bf79a147a32f15ba523919bd6cb22
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-06 19:50:49 -0300

    refactor: update countReducer to handle state and action, improve Counter component logic

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,17 @@
 
 import * as React from 'react'
 
-function countReducer(count, step) {
-  return count + step
+function countReducer(state, action) {
+  return {...state, ...action}
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => changeCount(step)
-  return <button onClick={increment}>{count}</button>
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => setState({count: count + step})
+  return <button onClick={increment}>{state.count}</button>
 }
 
 function App() {
